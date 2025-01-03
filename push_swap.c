e9f926e808201adadd92509b0c8b70ee10e98427
Ana Ramos Moya <anaana609@gmail.com>
2025-01-03 20:50:22 +0100

    Added rotate a, b function

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: aramos-m <aramos-m@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 21:21:34 by aramos-m          #+#    #+#             */
-/*   Updated: 2024/11/24 22:44:56 by aramos-m         ###   ########.fr       */
+/*   Updated: 2025/01/03 20:48:36 by aramos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,37 @@ Return values:
 - (<0): the element pointed by p1 goes before the element pointed by p2
 - (0): */
 
-void    sa(t_list **stacka)
+void    sab(t_list **stack)
 {
-    t_list tmp;
+    t_list  *tmp;
 
-    tmp = *stacka->next;
-    *stacka->next = *stacka->next->next;
-    tmp->next = *stacka;
-    **stacka = &tmp;
+    tmp = *stack->next;
+    *stack->next = *stack->next->next;
+    tmp->next = *stack;
+    **stack = &tmp;
 }
-void    pa(t_list **stacka, t_list **stackb)
+
+void    ss(t_list **stacka, t_list**stackb)
 {
-    t_list tmp;
+    sa(stacka);
+    sa(stackb);
+}
+
+void    pab(t_list **stack1, t_list **stack2)
+{
+    t_list *tmp;
     
-    tmp = stackb->next;
-    stackb->next = stacka;
-    stackb = tmp;
+    tmp = *stack2->next;
+    *stack2->next = *stack1;
+    stack2 = tmp;
+}
+
+void    rab(t_list **stack)
+{
+    t_list *tmp;
+
+    tmp = *stack->next;
+    (ft_lslast(*stack))->next = *stack;
+    *stack->next = NULL;
+    **stack = &tmp;
 }
